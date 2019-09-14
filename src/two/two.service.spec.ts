3a287b349d3bfcda31ebeb74edc9f1967600ba37
Failware <maximilian.obermayer@gmail.com>
2019-09-14 05:13:36 +0200

    added logging to test

--- src/two/two.service.spec.ts
+++ src/two/two.service.spec.ts
@@ -3,13 +3,15 @@ import { TwoService } from './two.service';
 import { OneService } from '../one/one.service';
 import { ThreeModule } from '../three/three.module';
 import { OneServiceMock } from '../classes';
+import { Logger } from '@nestjs/common';
 
 describe('TwoService', () => {
+  let module: TestingModule
   let service: TwoService;
   let oneService: OneService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
       imports: [ThreeModule],
       providers: [TwoService, {
         provide: OneService,
@@ -19,17 +21,29 @@ describe('TwoService', () => {
 
     service = module.get<TwoService>(TwoService);
     oneService = module.get<OneService>(OneService)
+    Logger.debug(oneService)
   });
+
+  afterAll(async () => {
+    await module.close()
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('should call OneServiceMock.doOneThing()', () => {
-    const originalSpy = jest.spyOn(oneService, 'doOneThing')
-
+  it('should spy on OneServiceMock.doOneThing()', () => {
+    // not working when spying on service from module.get()
+    // (it spies on actual OneService because oneService is an instance of OneService)
+    const spy = jest.spyOn(oneService, 'doOneThing')
     service.doTwoThing()
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
 
-    expect(originalSpy).toHaveBeenCalledTimes(1)
+  it('should call OneServiceMock.doOneThing()', () => {
+    // working when spying on mocked Service
+    const spy = jest.spyOn(OneServiceMock.prototype, 'doOneThing')
+    service.doTwoThing()
+    expect(spy).toHaveBeenCalledTimes(1)
   })
 });
